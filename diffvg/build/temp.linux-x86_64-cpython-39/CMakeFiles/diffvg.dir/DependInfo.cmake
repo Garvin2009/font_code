1e647041b43ec676223f28150f02af9bed2bb60f
林家弘 <82699511+Garvin2009@users.noreply.github.com>
2026-07-22 17:25:51 +0800

    Add files via upload

---
+++ diffvg/build/temp.linux-x86_64-cpython-39/CMakeFiles/diffvg.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/cg/FontCLIP/diffvg/atomic.cpp" "CMakeFiles/diffvg.dir/atomic.cpp.o" "gcc" "CMakeFiles/diffvg.dir/atomic.cpp.o.d"
+  "/home/cg/FontCLIP/diffvg/color.cpp" "CMakeFiles/diffvg.dir/color.cpp.o" "gcc" "CMakeFiles/diffvg.dir/color.cpp.o.d"
+  "/home/cg/FontCLIP/diffvg/parallel.cpp" "CMakeFiles/diffvg.dir/parallel.cpp.o" "gcc" "CMakeFiles/diffvg.dir/parallel.cpp.o.d"
+  "/home/cg/FontCLIP/diffvg/shape.cpp" "CMakeFiles/diffvg.dir/shape.cpp.o" "gcc" "CMakeFiles/diffvg.dir/shape.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ diffvg/build/temp.linux-x86_64-cpython-39/CMakeFiles/diffvg.dir/atomic.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/diffvg.dir/atomic.cpp.o: /home/cg/FontCLIP/diffvg/atomic.cpp \
+ /home/cg/miniconda3/envs/fontclip/x86_64-conda-linux-gnu/sysroot/usr/include/stdc-predef.h
